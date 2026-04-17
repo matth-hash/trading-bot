655b94e7c6949ac9683cc800d2bbc5cacb31b6cc
matth-hash <matthieucollon@gmail.com>
2026-04-17 19:46:16 +0200

    Create prime_algo.py

---
+++ core/prime_algo.py
@@ -0,0 +1,22 @@
+from core.data_collector import get_market_data
+from core.macro_filter import get_macro_bias
+from core.ml_model import compute_score
+from core.strategy import TradingStrategy
+
+def run_scan():
+    market_data = get_market_data()
+    macro = get_macro_bias()
+    strategy = TradingStrategy(market_data.keys())
+    results = []
+
+    for symbol, data in market_data.items():
+        signal = strategy.generate_signal(symbol, data)
+        score = compute_score(data, macro)
+        if signal != "HOLD" and score > 75:
+            results.append({
+                "symbol": symbol,
+                "signal": signal,
+                "score": score
+            })
+
+    return results
